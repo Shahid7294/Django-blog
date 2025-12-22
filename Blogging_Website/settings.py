bd6196488307de4cc38f07e57a463d3c3f6e243a
Shahid7294 <sahidzoo123@gmail.com>
2025-12-22 20:24:19 +0530

    blog app and blog model

--- Blogging_Website/settings.py
+++ Blogging_Website/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'blogs',
     'home',
 ]
 
@@ -117,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
